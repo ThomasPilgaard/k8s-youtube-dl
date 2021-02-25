27b08dd4bd86ec9ba75bf221c1438976be473bbf
Thomas Pilgaard Nielsen <thomas@homesurf.dk>
2021-02-25 22:56:51 +0100

    Styling changes for mobile

--- webservice/static/js/youtubedownload.js
+++ webservice/static/js/youtubedownload.js
@@ -1,5 +1,18 @@
 $(document).ready( function () {
     $('#jobs-table').DataTable({
         "bLengthChange": false,
+        rowReorder: {
+            selector: 'td:nth-child(0)'
+        },
+        responsive: true,
+        "order": [[ 3, "desc" ]],
+    });
+
+    $('#workers-table').DataTable({
+        "bLengthChange": false,
+        rowReorder: {
+            selector: 'td:nth-child(0)'
+        },
+        responsive: true,
     });
 });
